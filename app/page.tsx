b7e2fc825f46ec6c05e52eada941f91059b8a22c
Manish <my79220@gmail.com>
2023-12-07 22:07:03 +0530

    added

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-import Image from 'next/image'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
 import Footer from './Components/Footer'
-import Masonary from './Components/FAQ'
 import FAQ from './Components/FAQ'
 import Team from './Components/Team'
-import Bento from './Components/Bento'
-import Page404 from './Components/Page404'
+import Services from './Components/Services'
 export default function Home() {
   return (
     <>
@@ -14,7 +11,7 @@ export default function Home() {
     <div className='bg-gradient-to-t   from-[#030303] to-[#363636] '>
     <Hero/>
    
-    
+    <Services/>
   
    
     <Team/>
